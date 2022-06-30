0a9cd454960989452155541d4f441eeceecdc315
shakerbaker78 <bakeamym@yahoo.com>
2022-06-29 19:59:42 -0600

    added a home route so I could test OAuth

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,8 @@
 const routes = require('express').Router();
 const path = require('path');
 
-routes.use('/', require('./swagger'));
+routes.use('/', require('./home'));
+routes.use('/api-docs', require('./swagger'));
 routes.use('/users', require('./users'));
 routes.use('/photos', require('./photos'));
 routes.use('/albums', require('./albums'));
