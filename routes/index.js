98c4d6d846faef7ca17a2b90ae84278608454c1f
shakerbaker78 <bakeamym@yahoo.com>
2022-06-29 21:22:59 -0600

    implementing OAuth and a profile route

--- routes/index.js
+++ routes/index.js
@@ -7,5 +7,6 @@ routes.use('/users', require('./users'));
 routes.use('/photos', require('./photos'));
 routes.use('/albums', require('./albums'));
 routes.use('/clips', require('./clips'));
+routes.use('/profile', require('./profile'));
 
 module.exports = routes;
--- server.js
+++ server.js
@@ -4,12 +4,37 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 require('dotenv').config();
 
+
+//AuthO code
+const { auth } = require('express-openid-connect');
+
+const config = {
+  authRequired: false,
+  auth0Logout: true,
+  secret: process.env.SECRET,
+  baseURL: process.env.BASE_URL,
+  clientID: process.env.CLIENT_ID,
+  issuerBaseURL: process.env.ISSUER_BASE_URL
+};
+
+// auth router attaches /login, /logout, and /callback routes to the baseURL
+app.use(auth(config));
+
+// req.isAuthenticated is provided from the auth router
+app.get('/', (req, res) => {
+  res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
+});
+//end AuthO code
+
+
 //Mongoose schemas
 require('./models/users')
 require('./models/clips')
 require('./models/photos')
 require('./models/albums')
 
+
+//Connecting to mongoDB with mongoose
 mongoose.connect(process.env.MONGO_URI).then(() => {
    app.use(bodyParser.json()).use('/', require('./routes/index'));
 
