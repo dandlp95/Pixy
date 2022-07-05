025e55f0e72f1f1dd71e64703270c7160c564356
dandlp95 <del17011@byui.edu>
2022-07-04 23:18:11 -0600

    cleaned all controllers, fixed error handling and created shared controllers for clips and photos

---
+++ controllers/mediaController.js
@@ -0,0 +1,126 @@
+// Since clips and photographs will have the exact same logic for
+// the controllers, I made a single controller that can  be used for both.
+
+// Additionally, it can also be used for other controllers that may share
+// the same logic
+
+const {
+  Api400Error,
+  Api404Error,
+  Api401Error,
+} = require("../middleware/error-handling/ApiErrors");
+
+operations = {};
+
+// Create new clip
+operations.add = (Schema) => {
+  return (req, res, next) => {
+    try {
+      const newDoc = new Schema(req.body);
+      newDoc
+        .save()
+        .then((doc) => {
+          console.log("new document added");
+          res.json(doc);
+        })
+        .catch((err) => {
+          const error = new Api400Error(err.message);
+          next(error);
+        });
+    } catch (err) {
+      next(err);
+    }
+  };
+};
+
+// Get all clips
+operations.getDocs = (Schema) => {
+  return async (req, res, next) => {
+    try {
+      const docs = await Schema.find();
+      if (docs) {
+        res.status(200).json(docs);
+        console.log("All docs returned");
+      } else {
+        const err404 = new Api404Error("Not found.");
+        next(err404);
+      }
+    } catch (err) {
+      next(err);
+    }
+  };
+};
+
+// Get Clip By Id
+operations.getDocById = (Schema) => {
+  return async (req, res, next) => {
+    try {
+      const doc = await Schema.findById(req.params.id);
+      if (doc) {
+        res.status(200).json(doc);
+        console.log(`One doc: ${req.params.id}!`);
+      } else {
+        const err404 = new Api404Error("Not found.");
+        next(err404);
+      }
+    } catch (err) {
+      next(err);
+    }
+  };
+};
+
+// Edit clip by id
+operations.editMedia = (Schema) => {
+  return (req, res, next) => {
+    try {
+      //const clipId = new ObjectId(req.params.id);
+      const media = {
+        name: req.body.name,
+        clipDescription: req.body.clipDescription,
+        encodedClip: req.body.encodedClip,
+        location: req.body.location,
+        user: req.body.user,
+        cameraUsed: req.body.cameraUsed,
+        tags: req.body.tags,
+      };
+
+      for (field in media) {
+        if (field === null) {
+          delete field;
+        }
+      }
+
+      Schema.findByIdAndUpdate(req.params.id, media, (err, doc) => {
+        if (err) {
+          const err400 = new Api400Error(err.message);
+          next(err400);
+        } else {
+          console.log(doc);
+          res.status(200).json(doc);
+        }
+      });
+    } catch (err) {
+      next(err);
+    }
+  };
+};
+
+// Delete clip by id
+operations.delete = (Schema) => {
+  return async (req, res, next) => {
+    Schema.findByIdAndDelete(req.params.id, (err, doc) => {
+      if (err) {
+        const err400 = new Api400Error(err.message);
+        next(err400);
+      } else {
+        if (doc === null) {
+          res.status(200).send("Already Deleted.");
+        } else {
+          res.status(200).json(doc);
+        }
+      }
+    });
+  };
+};
+
+module.exports = operations;
--- controllers/users-controller.js
+++ controllers/users-controller.js
@@ -9,33 +9,34 @@ const {
 } = require("../middleware/error-handling/ApiErrors");
 
 // Create new user
-exports.addUser = (req, res) => {
+exports.addUser = (req, res, next) => {
   try {
     const user = new User(req.body);
     user
       .save()
       .then((user) => {
+        console.log("new user added");
         res.json(user);
       })
       .catch((err) => {
-        throw new Api400Error("Bad request.");
+        const error = new Api400Error(err.message);
+        next(error);
       });
-    console.log("new user added");
   } catch (err) {
     next(err);
   }
 };
 
 // Get all users
-exports.getUsers = async (req, res) => {
+exports.getUsers = async (req, res, next) => {
   try {
     const users = await User.find();
-
     if (users) {
       res.status(200).json(users);
       console.log("All Users! From the usersController file.");
     } else {
-      throw new Api404Error("Not found.");
+      const err404 = new Api404Error("Not found.");
+      next(err404);
     }
   } catch (err) {
     next(err);
@@ -43,18 +44,15 @@ exports.getUsers = async (req, res) => {
 };
 
 // Get User By Id
-exports.getUser = async (req, res) => {
+exports.getUser = async (req, res, next) => {
   try {
-    const userId = new ObjectId(req.params.id);
-    const result = await User.find({
-      _id: userId,
-    });
-
+    const result = await User.findById(req.params.id);
     if (result) {
-      res.status(200).json(result[0]);
+      res.status(200).json(result);
       console.log(`One User: ${req.params.id}! From the userController file.`);
     } else {
-      throw new Api404Error("Album not found.");
+      const err404 = new Api404Error("Not found.");
+      next(err404);
     }
   } catch (err) {
     next(err);
@@ -62,9 +60,8 @@ exports.getUser = async (req, res) => {
 };
 
 // Edit user by id
-exports.editUser = async (req, res) => {
+exports.editUser = (req, res, next) => {
   try {
-    const userId = new ObjectId(req.params.id);
     const user = {
       firstName: req.body.firstName,
       lastName: req.body.lastName,
@@ -72,49 +69,38 @@ exports.editUser = async (req, res) => {
       password: req.body.password,
     };
 
-    const result = await User.replaceOne(
-      {
-        _id: userId,
-      },
-      user
-    );
-
-    if (result) {
-      res.status(200).json(result[0]);
-      console.log(
-        `Modified User: ${req.params.id}! From the userController file.`
-      );
-    } else {
-      res
-        .status(500)
-        .json(response.error || "An error occurred while modifing the user.");
+    for (field in user) {
+      if (field === null) {
+        delete field;
+      }
     }
 
-    console.log(user);
+    User.findByIdAndUpdate(req.params.id, media, (err, doc) => {
+      if (err) {
+        const err400 = new Api400Error(err.message);
+        next(err400);
+      } else {
+        console.log(doc);
+        res.status(200).json(doc);
+      }
+    });
   } catch (err) {
     next(err);
   }
 };
 
 // Delete user by id
-exports.deleteUser = async (req, res) => {
-  try {
-    const userId = new ObjectId(req.params.id);
-    const result = await User.deleteOne({
-      _id: userId,
-    });
-
-    if (result) {
-      res.status(200).json(result[0]);
-      console.log(
-        `Deleted User: ${req.params.id}! From the userController file.`
-      );
+exports.deleteUser = (req, res, next) => {
+  User.findByIdAndDelete(req.params.id, (err, doc) => {
+    if (err) {
+      const err400 = new Api400Error(err.message);
+      next(err400);
     } else {
-      res
-        .status(500)
-        .json(response.error || "An error occurred while deleting the user.");
+      if (doc === null) {
+        res.status(200).send("Already Deleted.");
+      } else {
+        res.status(200).json(doc);
+      }
     }
-  } catch (err) {
-    next(err);
-  }
+  });
 };
